320d1fc86a5ccd1ae91ef6bc6f6b021c37dd8a21
vvghelu <vivekghelani11111@gmail.com>
2016-10-11 02:10:09 -0400

    Update calc.R

--- calc.R
+++ calc.R
@@ -1,2 +1,11 @@
-hi
-# gf
+library(data.table)
+
+df <- read.csv('Lightning Data.csv')
+df[is.na(df)] <- 0
+dT <- data.table(df)
+
+#### SLE(Putting 1 instead of 0 in location count)
+dfsle <- dT[Tariff.Cd == "ELE"]
+dfsle1 <- dfsle[Loc.Ct == "0"]
+dfsle1$Loc.Ct[dfsle1$Loc.Ct == 0] <- 1
+head(dfsle1)
